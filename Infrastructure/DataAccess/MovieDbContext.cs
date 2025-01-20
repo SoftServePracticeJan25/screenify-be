bdb582ecdaa259bde712e985e2981e0a03e22be5
Viacheslav Melnyk <altt1m.zaskar@gmail.com>
2025-01-21 00:38:38 +0200

    Update MovieDbContext

--- Infrastructure/DataAccess/MovieDbContext.cs
+++ Infrastructure/DataAccess/MovieDbContext.cs
@@ -11,4 +11,25 @@ public class MovieDbContext: DbContext
     }
     
     public DbSet<Movie> Movies { get; set; }
+    public DbSet<MovieActor> MovieActors { get; set; } // join table
+    public DbSet<Actor> Actors { get; set; }
+    public DbSet<ActorRole> ActorRoles { get; set; }
+    public DbSet<MovieGenre> MovieGenres { get; set; } // join table
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Session> Sessions { get; set; }
+    public DbSet<Ticket> Tickets { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<CinemaType> CinemaTypes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Composite Key
+        modelBuilder.Entity<MovieActor>()
+            .HasKey(e => new { e.MovieId, e.ActorId, e.ActorRoleId });
+
+        modelBuilder.Entity<MovieGenre>()
+            .HasKey(e => new { e.MovieId, e.GenreId });
+    }
+
 }
