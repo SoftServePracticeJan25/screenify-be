6201b8964f912298764d837338eddec5c236193c
Michael <135025465+T8mpest@users.noreply.github.com>
2025-02-05 03:43:18 +0100

    Fixed all necessary warnings and improved code stability

--- Infrastructure/Services/ReviewService.cs
+++ Infrastructure/Services/ReviewService.cs
@@ -42,11 +42,11 @@ namespace Infrastructure.Services
             reviews = reviews.Where(r => r.MovieId == query.MovieId);
         }
 
-        // Does filtration if appUser id in query is not null
-        if(!string.IsNullOrEmpty(query.AppUserId))
-        {
-            reviews = reviews.Where(r => r.AppUserId.Contains(query.AppUserId));
-        }
+         // Does filtration if appUser id in query is not null
+         if (!string.IsNullOrEmpty(query.AppUserId))
+         {
+            reviews = reviews.Where(r => r.AppUserId != null && r.AppUserId.Contains(query.AppUserId));
+         }
 
             var reviewList = await reviews.ToListAsync(); // Now working async
             return _mapper.Map<List<ReviewReadDto>>(reviewList);
