8287e9a701364b089a90e44fadb8a4e93d417e1f
Viacheslav Melnyk <altt1m.zaskar@gmail.com>
2025-02-06 09:55:40 +0200

    Add AvatarController

---
+++ Presentation/Controllers/AvatarController.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class AvatarController : ControllerBase
+    {
+        private readonly IAvatarService _avatarService;
+
+        public AvatarController(IAvatarService avatarService)
+        {
+            _avatarService = avatarService;
+        }
+
+        [HttpPost("upload-avatar")]
+        public async Task<IActionResult> UploadAvatar(IFormFile file)
+        {
+            try
+            {
+                var fileUrl = await _avatarService.UploadAvatarAsync(file, User);
+                return Ok(new { avatarUrl = fileUrl });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
+        }
+
+        [HttpGet("get-avatar-url")]
+        public async Task<IActionResult> GetAvatar()
+        {
+            var avatarUrl = await _avatarService.GetAvatarUrlAsync(User);
+            if (string.IsNullOrEmpty(avatarUrl))
+                return NotFound("Avatar not found.");
+
+            return Ok(new { avatarUrl });
+        }
+    }
+
+}
